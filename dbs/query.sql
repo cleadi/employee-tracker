5ce0491b5ab833b62011bf50702165b9bcdbb4e7
Dillon Cleaver <cleadi@fastmail.com>
2021-10-26 18:37:03 -0500

    Started to flesh out the js functions and inquirer questions.

--- dbs/query.sql
+++
@@ -1,4 +0,0 @@
-USE employee_tracker;
-
--- SELECT * FROM movies;
--- JOIN reviews ON movies.id = reviews.movie_id;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const cTable = require('console.table')
 const express = require('express')
+const inquirer = require('inquirer')
 const mysql = require('mysql2')
 
 const PORT = process.env.PORT || 3001
@@ -8,7 +9,25 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const connection = mysql.createConnection(
+const questions = [
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department',
+      'Quit'
+    ]
+  },
+  ///...
+]
+
+const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
@@ -19,26 +38,38 @@ const connection = mysql.createConnection(
 )
 
 function addEmployee() {
-  //...
+  db.query(`GET ALL THE ROLES`, (err, results) => {
+  //... results (array of roles)
+  })
 }
 
 function addRole() {
   //...
 }
 
-function viewEmployees() {
-  //... run the db query, get results, display results with console.table
+function viewAllEmployees() {
+  // run the db query, get results, display results with console.table
+  `SELECT * FROM employee
+    LEFT JOIN roles ON emp_role_id = ...
+    LEFT JOIN departments ON...`
+}
+// STOPPED HERE ON TUESDAY EVENING --> add functions for each selection?
+// --> run query code inside those functions?
+function viewAllDepartments() {
+  // run the db query, get results, display results with console.table
   `SELECT * FROM employee
     LEFT JOIN roles ON emp_role_id = ...
     LEFT JOIN departments ON...`
 }
 
 function init() {
-  prompt([
-    //... inital questions
-  ])
+  inquirer
+    .prompt(questions)
+    //...
 }
 
 app.listen(PORT, () => {
-  console.log('App listening at http://localhost:3001')
+  console.log(`App listening at http://localhost:${PORT}`)
 })
+
+init()
